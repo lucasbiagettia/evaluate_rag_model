136e699bb76f9450d465e89dfb2788d2bd5a4abc
lucasbiagetti <lucasbiagetti@gmail.com>
2024-03-25 22:01:16 -0300

    more cases

--- groq_model.py
+++ groq_model.py
@@ -12,7 +12,7 @@ class Chatbot(DeepEvalBaseLLM):
 
     def initialize(self, api_key):
         chat = ChatGroq(groq_api_key=api_key, model_name= "mixtral-8x7b-32768", temperature=0.2, verbose=True)
-        system = "Responderas a una api que testea otro modelo"
+        system = "Responderas a una api que testea otro modelo. Siempre debes responder en español"
         human = "{human}"
         prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
 
@@ -28,7 +28,8 @@ class Chatbot(DeepEvalBaseLLM):
     def get_model_name(self):
         return "Custom model"
     async def a_generate(self, prompt: str) -> str:
-        await self.generate(prompt)
+        ret  = self.chain.invoke({"human": prompt})
+        return ret['text']
 
 
 
